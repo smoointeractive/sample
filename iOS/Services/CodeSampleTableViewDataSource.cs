2407aa91ea9c9a56153bf69940c3bea8f90242ad
Sacha Moo <sacha@smoointeractive.com>
2017-06-08 06:19:31 -0700

    Clean to tableview and project structure.

--- iOS/Services/CodeSampleTableViewDataSource.cs
+++ iOS/Services/CodeSampleTableViewDataSource.cs
@@ -7,26 +7,20 @@ namespace CodeSample
 	{
 		SampleMovieListing sampleMovieListing;
 		string cellIdentifier = "sampledatacell";
-		string titleCellIdentifier = "sampleMovieTitle";
-		string descriptioCellIdentifier = "sampleMovieDescription";
 
 		public CodeSampleTableViewDataSource(SampleMovieListing data)
 		{
-			//networkRequestService = (NetworkRequestService)service;
 			sampleMovieListing = data;
-			// get data
-			System.Diagnostics.Debug.WriteLine("tick");
-			System.Diagnostics.Debug.WriteLine(sampleMovieListing.Title);
 		}
 
 		public override UITableViewCell GetCell(UITableView tableView, 
 		                                        NSIndexPath indexPath)
 		{
-			System.Diagnostics.Debug.WriteLine("spaced");
 			UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifier);
-			string item = sampleMovieListing.Movies[indexPath.Row].Title;
+			string item = String.Format("{0} ({1})", 
+			                            sampleMovieListing.Movies[indexPath.Row].Title,
+			                            sampleMovieListing.Movies[indexPath.Row].ReleaseYear);
 			cell.TextLabel.Text = item;
-
 
 			if (cell == null)
 			{
@@ -34,16 +28,20 @@ namespace CodeSample
 				                           cellIdentifier);
 			}
 
-
-
 			return cell;
 		}
 
 		public override nint RowsInSection(UITableView tableview, 
 		                                   nint section)
 		{
-			System.Diagnostics.Debug.WriteLine("Helooesansnak "+sampleMovieListing.Movies.Length);
 			return sampleMovieListing.Movies.Length;
+		}
+
+		public override string TitleForHeader(UITableView tableView, nint section)
+		{
+			return String.Format("{0} \n {1}", 
+			                     sampleMovieListing.Title,
+			                     sampleMovieListing.Description);
 		}
 	}
 }
